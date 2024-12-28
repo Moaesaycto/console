d6a61749d8099432e9739385c9e0cf3f7966e64f
Moaesaycto <stephen.lerantges.los@gmail.com>
2024-12-29 00:52:18 +1100

    Removed broken characters

--- src/commands/jokeCommand.ts
+++ src/commands/jokeCommand.ts
@@ -7,9 +7,9 @@ export const jokeCommand: Command = {
     parseParams: () => ({}),
     run: () => {
       const jokes = [
-        "Why don’t skeletons fight each other? They don’t have the guts.",
-        "What do you call cheese that isn’t yours? Nacho cheese.",
-        "Why couldn’t the bicycle stand up by itself? It was two-tired.",
+        "Why don't skeletons fight each other? They don't have the guts.",
+        "What do you call cheese that isn't yours? Nacho cheese.",
+        "Why couldn't the bicycle stand up by itself? It was two-tired.",
         "I told my wife she was drawing her eyebrows too high. She looked surprised.",
       ];
       const joke = jokes[Math.floor(Math.random() * jokes.length)];
