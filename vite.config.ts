3cf3d4b5dd02dd9083bf15797d2bda9e48088da3
Moaesaycto <stephen.lerantges.los@gmail.com>
2024-12-28 21:28:29 +1100

    Fixing config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/console/', // Use your GitHub repository name here
+  build: {
+    outDir: 'dist', // Optional: Ensure output goes to the 'dist' folder
+  },
+});
